3f23537984354c7e1d6fecfa23dd9a47d75cf58c
wwwmo <wwwmostafagmer100@gmail.com>
2024-05-18 23:33:34 +0300

    Torikkusu V4.6

--- lib/Torikkusu_Home_Page/Torikkusu_Home_Page_TitleText.dart
+++ lib/Torikkusu_Home_Page/Torikkusu_Home_Page_TitleText.dart
@@ -1,57 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class TitleText {
 
-
-  static TextStyle Sensei_Home_Japan_Font(BuildContext context) {
-    return GoogleFonts.yujiSyuku(
-        color: Theme.of(context).colorScheme.primary,
-        fontSize: 20,
-        fontWeight: FontWeight.bold
-
-    );
-  }
-
-
-
-
-  static TextStyle Sensei_Home_Romiji_Font (BuildContext context){
-    return GoogleFonts.overlock(
-      color: Theme.of(context).colorScheme.secondary,
-      fontSize:20,
+  static Sensei_Home_Japan_Font(BuildContext context) {
+    return TextStyle(
+        color: Theme
+            .of(context)
+            .colorScheme
+            .primary,
+        fontFamily: 'YujiSyuku',
+        fontSize: 25,
         fontWeight: FontWeight.bold
     );
   }
 
-  static TextStyle Sensei_Home_Button_Font(BuildContext context){
-    return GoogleFonts.overlock(
-    color: Theme.of(context).colorScheme.background,
-    fontSize:20
-    );
-  }
-
-  static TextStyle Sensei_Game_Header_Font(BuildContext context){
-    return GoogleFonts.overlock(
-        color: Theme.of(context).colorScheme.primary,
-        fontSize:20,
+  static Sensei_Home_Romiji_Font(BuildContext context) {
+    return TextStyle(
+        color: Theme
+            .of(context)
+            .colorScheme
+            .secondary,
+        fontFamily: 'Overlock',
+        fontSize: 25,
         fontWeight: FontWeight.bold
     );
   }
-  static TextStyle Sensei_Game_Player_Font(BuildContext context){
-    return GoogleFonts.overlock(
-        color: Theme.of(context).colorScheme.surface,
-        fontSize:20,
-        fontWeight: FontWeight.bold,
+
+  static Sensei_Home_Button_Font(BuildContext context) {
+    return TextStyle(
+        color: Theme
+            .of(context)
+            .colorScheme
+            .background,
+        fontFamily: 'Overlock',
+        fontSize: 25,
     );
   }
 
-  static TextStyle Sensei_Game_Score_Font(BuildContext context){
-    return GoogleFonts.overlock(
-        color: Theme.of(context).colorScheme.surface,
-        fontSize:20
+  static Sensei_Game_Header_Font(BuildContext context) {
+    return TextStyle(
+        color: Theme
+            .of(context)
+            .colorScheme
+            .primary,
+        fontFamily: 'Overlock',
+        fontSize: 25,
     );
   }
 
+  static Sensei_Game_Player_Font(BuildContext context) {
+    return TextStyle(
+        color: Theme
+            .of(context)
+            .colorScheme
+            .surface,
+        fontFamily: 'Overlock',
+        fontSize: 25,
+        fontWeight: FontWeight.bold
+    );
+  }
 
+  static Sensei_Game_Score_Font(BuildContext context) {
+    return TextStyle(
+        color: Theme
+            .of(context)
+            .colorScheme
+            .secondary,
+        fontFamily: 'Overlock',
+        fontSize: 25,
+    );
+  }
 }
